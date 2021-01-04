4b6d761b5a5fedba38e55b9eb0cebadf45bb367d
Jon Trowbridge <tro19008@byui.edu>
2021-01-04 10:26:20 -0500

    Add project files.

---
+++ MyFirstConsoleApplication/GlazerApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstConsoleApplication
+{
+    static class GlazerApp
+    {
+        public static void RunExample()
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("What is the width of the window? ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("What is the height of the window? ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+
+            glassArea = 2 * (width + height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet.");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres.");
+        }
+    }
+}
---
+++ MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyFirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.GetUserNameAndLocation();
+            p.ChristmasCountdown(DateTime.Now);
+            GlazerApp.RunExample();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private void GetUserNameAndLocation() {
+            Person person = new Person();
+            Console.Write("What is your name? ");
+            person.name = Console.ReadLine();
+            Console.Write($"Hi {person.name}! Where are you from? ");
+            person.location = Console.ReadLine();
+            Console.WriteLine($"I have never been to {person.location}. I bet it is nice. Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private void ChristmasCountdown(DateTime date)
+        {       
+            var christmas = new DateTime(2021, 12, 25);
+            Console.WriteLine($"Today's date is: {date.ToString("D", CultureInfo.CreateSpecificCulture("en-US"))}");
+            TimeSpan countdown = christmas - date.Date;
+            Console.WriteLine($"There are {countdown.TotalDays} days until Christmas! Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
